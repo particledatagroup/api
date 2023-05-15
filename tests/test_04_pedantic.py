4592e6b57def6eb6331a122844ab22dd5e88b3db
Juerg Beringer <juerg.beringer@gmail.com>
2023-05-15 12:51:32 -0700

    Add tests for pedantic mode

---
+++ tests/test_04_pedantic.py
@@ -0,0 +1,22 @@
+"""
+Test cases for PdgParticle.
+"""
+from __future__ import print_function
+
+import unittest
+
+import pdg
+from pdg.errors import PdgAmbiguousValueError
+
+
+class TestData(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.api = pdg.connect(pedantic=True)
+
+    def test_properties(self):
+        self.assertRaises(PdgAmbiguousValueError, getattr, self.api.get('Q007'), 'mass')
+
+    def test_summary(self):
+        self.assertRaises(PdgAmbiguousValueError, self.api.get('S013D').best_summary)
