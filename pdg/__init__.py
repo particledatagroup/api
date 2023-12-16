98e86b254bd3545632c2af8de61b3f35922e3493
Juerg Beringer <juerg.beringer@gmail.com>
2023-12-15 16:52:42 -0800

    Switch development version to 0.0.7; increase MIN_SCHEMA_VERSION in preparation for PDGPARTICLE schema change

--- pdg/__init__.py
+++ pdg/__init__.py
@@ -10,7 +10,7 @@ For general information about PDG and the Review of Particle Physics
 please visit https://pdg.lbl.gov.
 """
 __author__ = 'Particle Data Group'
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 
 
 import os
@@ -20,7 +20,7 @@ from pdg.errors import PdgApiError
 
 # Constants
 SQLITE_FILENAME = 'pdg.sqlite'      # Default SQLite database file used by this API
-MIN_SCHEMA_VERSION = 0.1            # Minimum schema version required by this version of the API
+MIN_SCHEMA_VERSION = 0.2            # Minimum schema version required by this version of the API
 
 
 def connect(database_url=None, pedantic=False):
