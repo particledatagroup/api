eb5363cf1329927726e2e709e426e09a37e250c2
Juerg Beringer <juerg.beringer@gmail.com>
2024-10-04 17:20:26 -0700

    Switch development version to 0.1.4

--- pdg/__init__.py
+++ pdg/__init__.py
@@ -10,7 +10,7 @@ For general information about PDG and the Review of Particle Physics
 please visit https://pdg.lbl.gov.
 """
 __author__ = 'Particle Data Group'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 
 import os
