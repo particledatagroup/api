2f076ccd4ac1d3ebad14e0166dcafd0b47716b87
Matt Kramer <mjkramer@users.noreply.github.com>
2025-05-01 17:04:32 -0700

    Modify print_datablock.py to tabulate the output

--- examples/print_datablock.py
+++ examples/print_datablock.py
@@ -3,26 +3,31 @@
 from itertools import chain
 import sys
 
+from tabulate import tabulate
+
 import pdg
 from pdg.api import PdgApi
 from pdg.measurement import PdgReference, PdgValue
 
 
-def print_value(v: PdgValue):
-    print(f'VALUE (10^{v.display_power_of_ten}): {v.value} ± {v.error}')
-    print(f'VALUE_TEXT: {v.display_value_text}')
-    print(f'EVTS: {v.measurement.event_count}')
-    print(f'DOCUMENT ID: {v.measurement.reference.document_id}')
-    print(f'TECN: {v.measurement.technique}')
-    print(f'COMMENT: {v.measurement.inline_comment}')
-    print()
+def dictify_value(v: PdgValue):
+    return {
+        'PWR_OF_TEN': f'10^{v.display_power_of_ten}',
+        'VALUE': f'{v.value} ± {v.error}',
+        'VALUE_TEXT': v.display_value_text,
+        'EVTS': v.measurement.event_count,
+        'DOCUMENT ID': v.measurement.reference.document_id,
+        'TECN': v.measurement.technique,
+        'COMMENT': v.measurement.inline_comment,
+    }
 
 
-def print_reference(ref: PdgReference):
-    print(f'ID: {ref.document_id}')
-    print(f'PUBLICATION: {ref.publication_name}')
-    print(f'TITLE: {ref.title}')
-    print()
+def dictify_reference(ref: PdgReference):
+    return {
+        'ID': ref.document_id,
+        'PUBLICATION': ref.publication_name,
+        'TITLE': ref.title,
+    }
 
 
 def print_datablock(api: PdgApi, pdgid: str):
@@ -42,36 +47,37 @@ def print_datablock(api: PdgApi, pdgid: str):
 
     vals1 = [v for v in values if v.used_in_fit]
     assert all(v.used_in_average for v in vals1)
+    vals1 = [dictify_value(v) for v in vals1]
 
     vals2 = [v for v in values if v.used_in_average and not v.used_in_fit]
+    vals2 = [dictify_value(v) for v in vals2]
 
     vals3 = [v for v in values if not (v.used_in_average or v.used_in_fit)]
+    vals3 = [dictify_value(v) for v in vals3]
 
     print('USED IN FITS AND AVERAGES')
     print('-------------------------')
-    for v in vals1:
-        print_value(v)
+    print(tabulate(vals1, headers='keys'))
 
     if vals2:
         print()
         print('USED IN AVERAGES BUT NOT FITS')
         print('-----------------------------')
-        for v in vals2:
-            print_value(v)
+        print(tabulate(vals2, headers='keys'))
 
     if vals3:
         print()
         print('NOT USED IN FITS OR AVERAGES')
         print('----------------------------')
-        for v in vals3:
-            print_value(v)
+        print(tabulate(vals3, headers='keys'))
 
+    print()
     print('REFERENCES')
     print('==========')
 
     refs = [m.reference for m in node.get_measurements()]
-    for ref in refs:
-        print_reference(ref)
+    refs = [dictify_reference(r) for r in refs]
+    print(tabulate(refs, headers='keys'))
 
 
 if __name__ == '__main__':
