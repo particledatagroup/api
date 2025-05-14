a2179329dee861a6d0f4c0e64d8f0781f0462ec9
Matt Kramer <mjkramer@users.noreply.github.com>
2025-05-14 10:40:24 -0700

    Update print_datablock.py

--- examples/print_datablock.py
+++ examples/print_datablock.py
@@ -7,14 +7,17 @@ from tabulate import tabulate
 
 import pdg
 from pdg.api import PdgApi
+from pdg.data import PdgSummaryValue
 from pdg.measurement import PdgReference, PdgValue
 
 
 def dictify_value(v: PdgValue):
     return {
+        'COLUMN_NAME': v.column_name,
         'PWR_OF_TEN': f'10^{v.display_power_of_ten}',
-        'VALUE': f'{v.value} ± {v.error}',
-        'VALUE_TEXT': v.display_value_text,
+        'DPY_VALUE_TEXT': v.display_value_text,
+        'VALUE_TEXT': v.value_text,
+        'VALUE_NUM': f'{v.value} ± {v.error}',
         'EVTS': v.measurement.event_count,
         'DOCUMENT ID': v.measurement.reference.document_id,
         'TECN': v.measurement.technique,
@@ -29,18 +32,27 @@ def dictify_reference(ref: PdgReference):
         'TITLE': ref.title,
     }
 
+def dictify_summval(sv: PdgSummaryValue):
+    return {
+        'VALUE_TYPE': sv.value_type,
+        'DPY_VALUE_TEXT': sv.display_value_text,
+        'VALUE_TEXT': sv.value_text,
+        'VALUE_NUM': f'{sv.value} ± {sv.error}'
+    }
+
 
 def print_datablock(api: PdgApi, pdgid: str):
     node = api.get(pdgid)
 
+    print()
     print(node.description)
     print()
 
-    for sv in node.summary_values():
-        print(f'{sv.value_type}: {sv.value} ± {sv.error}')
-        print(f'{sv.display_value_text}')
-        print()
-    print()
+    print('SUMMARY VALUES')
+    print('==============')
+
+    summvals = [dictify_summval(sv) for sv in node.summary_values()]
+    print(tabulate(summvals, headers='keys'))
 
     values = chain.from_iterable(m.values() for m in node.get_measurements())
     values = list(values)
@@ -55,20 +67,21 @@ def print_datablock(api: PdgApi, pdgid: str):
     vals3 = [v for v in values if not (v.used_in_average or v.used_in_fit)]
     vals3 = [dictify_value(v) for v in vals3]
 
-    print('USED IN FITS AND AVERAGES')
-    print('-------------------------')
+    print()
+    print('MEASUREMENTS USED IN FITS AND AVERAGES')
+    print('======================================')
     print(tabulate(vals1, headers='keys'))
 
     if vals2:
         print()
-        print('USED IN AVERAGES BUT NOT FITS')
-        print('-----------------------------')
+        print('MEASUREMENTS USED IN AVERAGES BUT NOT FITS')
+        print('==========================================')
         print(tabulate(vals2, headers='keys'))
 
     if vals3:
         print()
-        print('NOT USED IN FITS OR AVERAGES')
-        print('----------------------------')
+        print('MEASUREMENTS NOT USED IN FITS OR AVERAGES')
+        print('=========================================')
         print(tabulate(vals3, headers='keys'))
 
     print()
