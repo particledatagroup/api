5befcdba14f642d6927253f9c80eaae98060d98e
Matt Kramer <mjkramer@users.noreply.github.com>
2025-05-14 11:32:12 -0700

    Fix unit tests that assumed 2025 edition instead of 2024

--- tests/test_02_data.py
+++ tests/test_02_data.py
@@ -155,14 +155,14 @@ class TestData(unittest.TestCase):
         # Check that DISPLAY_VALUE_TEXT is being parsed properly
         # Symmetric errors:
         self.assertEqual(round(self.api.get('S086T').value*1e12, 2), 1.52)
-        self.assertEqual(round(self.api.get('S086T').error_positive*1e15, 1), 6)
-        self.assertEqual(round(self.api.get('S086T').error_negative*1e15, 1), 6)
-        self.assertEqual(round(self.api.get('S086T').error*1e15, 1), 6)
+        self.assertEqual(round(self.api.get('S086T').error_positive*1e15, 1), 5)
+        self.assertEqual(round(self.api.get('S086T').error_negative*1e15, 1), 5)
+        self.assertEqual(round(self.api.get('S086T').error*1e15, 1), 5)
         # Slightly asymmetric errors:
         self.assertEqual(round(self.api.get('S063T').value*1e12, 2), 1.64)
-        self.assertEqual(round(self.api.get('S063T').error_positive*1e13, 1), 1.6)
-        self.assertEqual(round(self.api.get('S063T').error_negative*1e13, 1), 1.6)
-        self.assertEqual(round(self.api.get('S063T').error*1e13, 1), 1.6)
+        self.assertEqual(round(self.api.get('S063T').error_positive*1e13, 1), 1.8)
+        self.assertEqual(round(self.api.get('S063T').error_negative*1e13, 1), 1.7)
+        self.assertEqual(round(self.api.get('S063T').error*1e13, 2), 1.75)
         # Very asymmetric errors:
         self.assertEqual(round(self.api.get('S041P63').value*1e3, 1), 10.2)
         self.assertEqual(round(self.api.get('S041P63').error_positive*1e3, 1), 3.2)
