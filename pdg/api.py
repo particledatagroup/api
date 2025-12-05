2d8af57b7da612ed646adc6e46d3790cad11288f
Matt Kramer <mjkramer@users.noreply.github.com>
2025-12-05 14:16:13 -0800

    Explicitly add a StreamHandler to the logger
    Not needed in Python 3.2+, but doesn't hurt either. Older Pythons will
    complain about a missing handler

--- pdg/api.py
+++ pdg/api.py
@@ -54,7 +54,11 @@ class PdgApi:
         for k in self.info_keys():
             setattr(self, k, self.info(k))
         self.pedantic = pedantic
+
         self.logger = logging.getLogger('PDG')
+        if not self.logger.handlers:
+            self.logger.addHandler(logging.StreamHandler())
+            self.logger.propagate = False
 
     def __str__(self):
         s = ['%s Review of Particle Physics, data release %s, API version %s' % (self.info('edition'),
