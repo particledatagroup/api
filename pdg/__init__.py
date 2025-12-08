d96ea4e79a410ab3227c4ff33e2a62d823aba6a3
Juerg Beringer <juerg.beringer@gmail.com>
2025-12-08 15:20:31 -0800

    Initialize version 0.2.3

--- pdg/__init__.py
+++ pdg/__init__.py
@@ -10,7 +10,7 @@ For general information about PDG and the Review of Particle Physics
 please visit https://pdg.lbl.gov.
 """
 __author__ = 'Particle Data Group'
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 
 import os
