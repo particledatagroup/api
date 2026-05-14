f6468bdd949a82b894b1915b13bb5594c87029c9
Matt Kramer <mjkramer@users.noreply.github.com>
2026-05-14 11:32:35 -0700

    Switch from viewcode to linkcode
    Motivated by the fact that viewcode can't link to the source of
    properties.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import inspect
 import sys
 import os
+from pathlib import Path
 sys.path.insert(0, os.path.abspath("../.."))
 
 project = 'PDG API'
@@ -21,7 +23,8 @@ extensions = [
     "myst_parser",
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
-    "sphinx.ext.viewcode",
+    # "sphinx.ext.viewcode",
+    "sphinx.ext.linkcode",
     "sphinx_rtd_theme",
 ]
 
@@ -41,3 +44,53 @@ html_static_path = ['_static']
 html_js_files = [
     'matomo.js'
 ]
+
+## The below concatenates the class and __init__ docstrings
+## (default is to just include the class's docstring)
+autoclass_content = 'both'
+
+## However, explicitly listing __init__ may be clearer
+# autodoc_default_options = {
+#     'members': True,
+#     'special-members': '__init__',
+#     'undoc-members': False,
+# }
+
+def linkcode_resolve(domain, info):
+    if domain != 'py':
+        return None
+    if not info['module']:
+        return None
+
+    module = sys.modules.get(info['module'])
+    if module is None:
+        return None
+
+    obj = module
+    for part in info['fullname'].split('.'):
+        try:
+            obj = getattr(obj, part)
+        except AttributeError:
+            return None
+
+    # Unwrap properties to get the underlying function
+    if isinstance(obj, property):
+        obj = obj.fget
+
+    try:
+        obj = inspect.unwrap(obj)
+        filename = inspect.getsourcefile(obj)
+        source, lineno = inspect.getsourcelines(obj)
+    except (TypeError, OSError):
+        return None
+
+    if filename is None:
+        return None
+
+    # Make path relative to the repo root
+    start = Path(__file__).parent.parent.parent
+    filename = os.path.relpath(filename, start=start)
+
+    repo = os.getenv('PDGAPI_REPO', 'particledatagroup/api')
+    branch = os.getenv('PDGAPI_BRANCH', 'main')
+    return f"https://github.com/{repo}/blob/{branch}/{filename}#L{lineno}-L{lineno + len(source) - 1}"
